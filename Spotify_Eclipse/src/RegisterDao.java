b9af35590a7789800063c151dd5430505caa463f
Steven Le <stevenl1@asu.edu>
2020-04-27 22:01:06 -0700

    Print statements to the console

--- Spotify_Eclipse/src/RegisterDao.java
+++ Spotify_Eclipse/src/RegisterDao.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RegisterDao {
@@ -43,6 +44,20 @@ public class RegisterDao {
             ps.setString(5, album.getRelease_date());
             ps.setString(6, genre.getGenreId());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from album");
+            while(query.next()) {
+                String albumId = query.getObject(1).toString();
+                String title = query.getObject(2).toString();
+                String artist = query.getObject(3).toString();
+                String track = query.getObject(4).toString();
+                String releaseDate = query.getObject(5).toString();
+                
+                System.out.println("\nAlbum ID: " + albumId + " Album Title: " +
+                title + " Artist: " + artist + " Track: " + track + " Release Date: " +
+                        releaseDate);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
@@ -61,6 +76,15 @@ public class RegisterDao {
             ps.setString(1, genre.getGenreId());
             ps.setString(2, genre.getType());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from genre");
+            while(query.next()) {
+                String genreId = query.getObject(1).toString();
+                String type = query.getObject(2).toString();
+                
+                System.out.println("\nGenre ID: " + genreId + " Type: " + type);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
@@ -81,6 +105,15 @@ public class RegisterDao {
             ps.setString(2, tracks.getTrackDur());
             ps.setString(3, album.getAlbum_id());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from tracks");
+            while(query.next()) {
+                String tName = query.getObject(1).toString();
+                String tDur = query.getObject(2).toString();
+                
+                System.out.println("\nTrack Name: " + tName + " Track Duration: " + tDur);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
@@ -102,6 +135,17 @@ public class RegisterDao {
             ps.setString(3, artist.getFans());
             ps.setString(4, tracks.getTrackName());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from artist");
+            while(query.next()) {
+                String artistId = query.getObject(1).toString();
+                String albumName = query.getObject(2).toString();
+                String fans = query.getObject(3).toString();
+                
+                System.out.println("\nArtist ID: " + artistId + " Album Name: " + albumName +
+                        " Fans: " + fans);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
@@ -120,6 +164,15 @@ public class RegisterDao {
             ps.setString(1, user.getUserId());
             ps.setString(2, user.getFollowers());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from user");
+            while(query.next()) {
+                String userId = query.getObject(1).toString();
+                String followers = query.getObject(2).toString();
+                
+                System.out.println("\nUser ID: " + userId + " Followers : " + followers);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
@@ -142,6 +195,18 @@ public class RegisterDao {
             ps.setString(4, playlist.getDateAdded());
             ps.setString(5, user.getUserId());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from playlist");
+            while(query.next()) {
+                String playlistId = query.getObject(1).toString();
+                String title = query.getObject(2).toString();
+                String duration = query.getObject(3).toString();
+                String added = query.getObject(4).toString();
+                
+                System.out.println("\nPlaylist ID: " + playlistId + " Playlist Title : " + title
+                        + " Playlist Duration: " + duration + " Date Added: " + added);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
@@ -162,6 +227,15 @@ public class RegisterDao {
             ps.setString(2, popularity.getLikes());
             ps.setString(3, tracks.getTrackName());
             ps.executeUpdate();
+            
+            ResultSet query = ps.executeQuery("SELECT * from popularity");
+            while(query.next()) {
+                String followers = query.getObject(1).toString();
+                String likes = query.getObject(2).toString();
+                
+                System.out.println("\nFollowers: " + followers + " Likes: " + likes);
+            }
+            
         } catch (SQLException e) {
             result = "Data Not Entered!";
             e.printStackTrace();
